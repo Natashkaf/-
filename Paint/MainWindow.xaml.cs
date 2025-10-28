fdb513fc8d0649c5cd1bc05c3455d1858c26703f
Natashkaf <125800181+Natashkaf@users.noreply.github.com>
2025-10-29 01:22:30 +1000

    Paint - минимум-версия умеет только рисовать курсором мышки, выбранными цветами

---
+++ Paint/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace Paint
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+            inkCanvas.DefaultDrawingAttributes.Width = 5;
+            inkCanvas.DefaultDrawingAttributes.Height = 5;
+        
+            // Режим рисования вкл всегда
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+        }
+//для рисования выбранным цветом 
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Background is SolidColorBrush brush)
+            {
+                inkCanvas.DefaultDrawingAttributes.Color = brush.Color;
+
+            }
+        }
+    }
+    
+}
